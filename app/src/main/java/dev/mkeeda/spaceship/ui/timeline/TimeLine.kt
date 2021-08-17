fc269d5a3c49d4baca422c124d6ee646d37d1ad5
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-17 23:23:06 +0900

    Fix spacer height between sender and body

--- app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -89,7 +90,7 @@ fun TimeLineRow(post: TimeLinePost) {
                     textAlign = TextAlign.End,
                 )
             }
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
             Text(text = post.body)
         }
     }
