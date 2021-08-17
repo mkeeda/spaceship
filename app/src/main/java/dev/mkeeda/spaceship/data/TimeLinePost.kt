d8ca19e18ddf53d23f927a07125487bb504ef4f2
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-17 23:13:26 +0900

    More sample data

--- app/src/main/java/dev/mkeeda/spaceship/data/TimeLinePost.kt
+++ app/src/main/java/dev/mkeeda/spaceship/data/TimeLinePost.kt
@@ -22,4 +22,19 @@ val fakeTimeLinePostItems = listOf(
         postTime = "2021/08/16 14:26",
         body = "どうして女の人だけが、自分の名字を捨てなきゃいけないのか",
     ),
+    TimeLinePost(
+        senderName = "森山みくり",
+        postTime = "2021/08/16 14:26",
+        body = "百合ちゃんの助けになれない。家のこともできない。仕事も行ってない。いいことひとつもない。うううう",
+    ),
+    TimeLinePost(
+        senderName = "土屋百合",
+        postTime = "2021/08/16 14:26",
+        body = "体の不調がきついのは、みんな同じ。比べるものじゃないでしょ",
+    ),
+    TimeLinePost(
+        senderName = "沼田頼綱",
+        postTime = "2021/08/16 14:26",
+        body = "誰が休んでも仕事が回る。帰ってこられる環境を普段から作っておくこと。それが職場におけるリスク管理",
+    ),
 )
