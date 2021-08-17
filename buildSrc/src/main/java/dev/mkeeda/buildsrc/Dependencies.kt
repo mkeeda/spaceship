d5bc2ceac2dd89912755e923faea0ba526a25d77
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-17 16:23:25 +0900

    Remove compose_version external variable

--- buildSrc/src/main/java/dev/mkeeda/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/dev/mkeeda/buildsrc/Dependencies.kt
@@ -7,7 +7,7 @@ object Libs {
         const val activityCompose = "androidx.activity:activity-compose:1.3.1"
 
         object Compose {
-            private const val version = "1.0.1"
+            const val version = "1.0.1"
             const val ui = "androidx.compose.ui:ui:$version"
             const val material = "androidx.compose.material:material:$version"
             const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$version"
