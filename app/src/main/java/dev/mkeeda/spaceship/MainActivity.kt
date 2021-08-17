48f976796799ed4a7856792402df341e52ff0639
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-17 23:09:04 +0900

    Make time line screen

--- app/src/main/java/dev/mkeeda/spaceship/MainActivity.kt
+++ app/src/main/java/dev/mkeeda/spaceship/MainActivity.kt
@@ -3,41 +3,17 @@ package dev.mkeeda.spaceship
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dev.mkeeda.spaceship.data.fakeTimeLinePostItems
 import dev.mkeeda.spaceship.ui.theme.SpaceshipTheme
+import dev.mkeeda.spaceship.ui.timeline.TimeLineScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SpaceshipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                TimeLineScreen(postItems = fakeTimeLinePostItems)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpaceshipTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +29,16 @@ import dev.mkeeda.spaceship.data.TimeLinePost
 import dev.mkeeda.spaceship.data.fakeTimeLinePostItems
 import dev.mkeeda.spaceship.ui.theme.Gray400
 import dev.mkeeda.spaceship.ui.theme.SpaceshipTheme
+
+@Composable
+fun TimeLineScreen(postItems: List<TimeLinePost>) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        TimeLine(postItems = postItems)
+    }
+}
 
 @Composable
 fun TimeLine(postItems: List<TimeLinePost>) {
