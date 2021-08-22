3ef1188d8b06c24ae810b6b012583c980acef6c3
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-22 16:40:53 +0900

    Style top app bar color

--- app/src/main/java/dev/mkeeda/spaceship/ui/foundation/SpaceshipAppBar.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/foundation/SpaceshipAppBar.kt
@@ -1,10 +1,37 @@
 package dev.mkeeda.spaceship.ui.foundation
 
+import android.content.res.Configuration
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.mkeeda.spaceship.ui.theme.SpaceshipTheme
 
 @Composable
 fun SpaceshipAppBar(currentScreen: Screen) {
-    TopAppBar(title = { Text(text = currentScreen.name) })
+    TopAppBar(
+        title = { Text(text = currentScreen.name) },
+        backgroundColor = MaterialTheme.colors.surface
+    )
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun LightAppBarPreview() {
+    SpaceshipTheme {
+        SpaceshipAppBar(currentScreen = Screen.TimeLine)
+    }
+}
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun DarkAppBarPreview() {
+    SpaceshipTheme {
+        SpaceshipAppBar(currentScreen = Screen.TimeLine)
+    }
 }
