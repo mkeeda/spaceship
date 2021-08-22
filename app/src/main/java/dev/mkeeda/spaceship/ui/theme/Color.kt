4c45e0aa8bc5f8545ef8959eb3177182da5698f6
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-22 16:40:19 +0900

    Define spaceship colors

--- app/src/main/java/dev/mkeeda/spaceship/ui/theme/Color.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package dev.mkeeda.spaceship.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Gray400 = Color(0xFFBDBDBD)
+val Gray900 = Color(0xFF131313)
+val Gray800 = Color(0xFF333333) // kintone gray
+val Gray400 = Color(0xFFACACAC)
+val Yellow400 = Color(0xFFFFCC00) // kintone yellow
+val Yellow200 = Color(0xFFFFE17C)
--- app/src/main/java/dev/mkeeda/spaceship/ui/theme/Theme.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/theme/Theme.kt
@@ -7,24 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Gray400,
+    primaryVariant = Gray900,
+    secondary = Yellow200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Gray800,
+    primaryVariant = Gray900,
+    secondary = Yellow400
 )
 
 @Composable
