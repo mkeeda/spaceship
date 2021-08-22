f738f62e5620ca40e506a325953cc026fdd979ae
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-08-22 13:38:49 +0900

    Extract sender icon composable

--- app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
+++ app/src/main/java/dev/mkeeda/spaceship/ui/timeline/TimeLine.kt
@@ -1,9 +1,7 @@
 package dev.mkeeda.spaceship.ui.timeline
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,11 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -23,14 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.mkeeda.spaceship.data.TimeLinePost
 import dev.mkeeda.spaceship.data.fakeTimeLinePostItems
-import dev.mkeeda.spaceship.ui.theme.Gray400
 import dev.mkeeda.spaceship.ui.theme.SpaceshipTheme
 
 @Composable
@@ -80,12 +74,8 @@ fun TimeLineRow(
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        // TODO: Sender icon
-        Box(
+        SenderIcon(
             modifier = Modifier
-                .size(48.dp)
-                .clip(CircleShape)
-                .background(Gray400) // FIXME: use theme color
                 .align(Alignment.Top)
         )
         Spacer(modifier = Modifier.width(16.dp))
