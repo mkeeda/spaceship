1eee630366feb4d82ffa9d25211b83be387d914d
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-10-03 13:22:47 +0900

    Update to navigation-compose 2.4.0-alpha10. navigation-compose requires compile SDK 31, so use compile and target SDK 31

--- buildSrc/src/main/java/dev/mkeeda/spaceship/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/dev/mkeeda/spaceship/buildsrc/Dependencies.kt
@@ -30,7 +30,7 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.6.0"
         const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1"
         const val activityCompose = "androidx.activity:activity-compose:1.3.1"
-        const val navigationCompose = "androidx.navigation:navigation-compose:2.4.0-alpha06"
+        const val navigationCompose = "androidx.navigation:navigation-compose:2.4.0-alpha10"
         const val constraintLayoutCompose = "androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02"
         const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
 
