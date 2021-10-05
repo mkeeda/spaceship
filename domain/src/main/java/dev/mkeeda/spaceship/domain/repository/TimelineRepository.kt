ed588e4db47383473061951de82c1e88f894bc12
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2021-10-05 23:46:37 +0900

    Define endpoint: /k/api/ntf/get.json

--- domain/src/main/java/dev/mkeeda/spaceship/domain/repository/TimelineRepository.kt
+++ domain/src/main/java/dev/mkeeda/spaceship/domain/repository/TimelineRepository.kt
@@ -1,7 +1,9 @@
 package dev.mkeeda.spaceship.domain.repository
 
+import dev.mkeeda.spaceship.data.PostId
 import dev.mkeeda.spaceship.data.TimelinePost
 
 interface TimelineRepository {
     suspend fun getTimelinePostList(): List<TimelinePost>
+    suspend fun getTimelinePost(postId: PostId): TimelinePost
 }
--- infra/src/main/java/dev/mkeeda/spaceship/infra/repositoryimpl/TimelineRepositoryImpl.kt
+++ infra/src/main/java/dev/mkeeda/spaceship/infra/repositoryimpl/TimelineRepositoryImpl.kt
@@ -5,6 +5,8 @@ import dev.mkeeda.spaceship.data.TimelinePost
 import dev.mkeeda.spaceship.data.kintone.KintoneNotificationList
 import dev.mkeeda.spaceship.domain.repository.TimelineRepository
 import dev.mkeeda.spaceship.infra.api.KintoneApiService
+import dev.mkeeda.spaceship.infra.api.ntf.NtfGet
+import dev.mkeeda.spaceship.infra.api.ntf.NtfGetResponse
 import dev.mkeeda.spaceship.infra.api.ntf.NtfList
 import javax.inject.Inject
 
@@ -21,6 +23,16 @@ class TimelineRepositoryImpl @Inject constructor(
         )
         return response.toTimeline()
     }
+
+    override suspend fun getTimelinePost(postId: PostId): TimelinePost {
+        val response = kintoneApiService.post<NtfGetResponse>(
+            endpoint = NtfGet,
+            param = NtfGet.RequestParam(
+                id = postId.value
+            )
+        )
+        return response.toTimeline()
+    }
 }
 
 private fun KintoneNotificationList.toTimeline(): List<TimelinePost> {
@@ -32,4 +44,13 @@ private fun KintoneNotificationList.toTimeline(): List<TimelinePost> {
             body = kintoneNotification.content.message.text
         )
     }
+}
+
+private fun NtfGetResponse.toTimeline(): TimelinePost {
+    return TimelinePost(
+        id = PostId(value = item.id),
+        senderName = sender?.name ?: "",
+        postTime = item.sentTime.toString(),
+        body = item.content.message.text
+    )
 }
