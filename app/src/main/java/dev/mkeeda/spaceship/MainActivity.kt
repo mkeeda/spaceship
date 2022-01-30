2c8a25755ac1c6cd01fd18f7ad5565f0f773c30f
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2022-01-30 15:04:42 +0900

    Fix preview error. The error cause is initialization of systemUiController.

--- app/src/main/java/dev/mkeeda/spaceship/MainActivity.kt
+++ app/src/main/java/dev/mkeeda/spaceship/MainActivity.kt
@@ -3,11 +3,15 @@ package dev.mkeeda.spaceship
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.mkeeda.spaceship.ui.common.theme.SpaceshipTheme
 import dev.mkeeda.spaceship.ui.foundation.AppNavigation
@@ -21,6 +25,14 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons
+                )
+            }
             SpaceshipTheme {
                 ProvideWindowInsets {
                     val navController = rememberNavController()
